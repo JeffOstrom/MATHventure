17f1b8cd407d12bf3fb5172f380deed8818c43c5
Denise Erazmus <denise.erazmus@gmail.com>
2018-02-24 16:48:21 -0600

    name form for new player react

--- src/pages/Newplayer.js
+++ src/pages/Newplayer.js
@@ -1,57 +1,105 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./assets/css/style.css"
+import ReactDOM from 'react-dom';
+import "./assets/css/style.css";
 import logo from "./assets/images/mv.png"
 
-const Newplayer = () =>
-    <div>
-        <section className="login-block">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4 login-sec">
-                        <h2 className="text-center">New Player</h2>
-                        <form class="newplayer-form">
-                            <div className="form-group">
-                                <label for="userName" className="text">Username</label>
-                                <input type="text" className="form-control" id="userName" placeholder="" required=""></input>
-                            </div>
-                            
-                            <div className="form-group">
-                                <label for="password" className="text">Password</label>
-                                <input type="password" className="form-control" id="newPassword" placeholder="" required=""></input>
-                            </div>
+class newPlayerForm extends React.Component {
 
-                            <div>
-                                <label for="confirmPassword" className="text">Confirm Password</label>
-                                <input type="password" className="form-control" id="confirmPassword" placeholder="" required="" onkeyup="checkPass(); return false;"></input>
-                            </div>
+       state={
+            name: '',
+            userName: '',
+            password: '',
+            age: '',
+            parentsEmail: ''
+        };
 
-                            <div>
-                                <label for="dob" className="date">Birthday</label>
-                                <input type="date" className="form-control" id="birthdate" placeholder="" required=""></input>
-                            </div>
+    //validatePassword = () => if name === 'password'
 
-                            <div>
-                                <label for="dob" className="text">Parents Email address</label>
-                                <input type="email" className="form-control" id="email" placeholder=""></input>
-                            </div>
+    handleInputChange = (event) => {
+        const {name, value} = event.target;
+       // this.validatePassword(name, value)
+        this.setState({
+            [name]: value
+        });
+    }
 
-                            <div className="form-check">
-                                <Link className="btn btn-login float-right" id="btnSubmit" value="Submit" type="submit" to="/mathtype">
-                                    Submit
-                                </Link>
-                                
+render(){
+    return (
+            <div>
+                <section className="login-block">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-4 login-sec">
+                                 <h2 className="text-center">New Player</h2>
+                                    <form className="newplayer-form">
+                                        <div className="form-group">
+                                            <label for="userName" className="text">Username
+                                                 <input
+                                                    name="userName"
+                                                    type= "text"
+                                                    onChange={this.handleInputChange}
+                                                    value={this.state.userName}/>
+                                            </label>
+                                        </div>
+                                     </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
-
-                    <div className="col-md-8 banner-sec">
-                        <img id="logo" src={logo} alt="mathventure"></img>
-                    </div>
-                </div>
+                </section>
             </div>
-        </section>
-    </div>
-;
+       );
+    }
+}
 
-export default Newplayer;
+   // <div>
+        //<section className="login-block">
+            //<div className="container">
+                //<div className="row">
+                    //<div className="col-md-4 login-sec">
+                       // <h2 className="text-center">New Player</h2>
+                       // <form class="newplayer-form">
+                           // <div className="form-group">
+                              //  <label for="userName" className="text">Username</label>
+                              //  <input type="text" className="form-control" id="userName" placeholder="" required=""></input>
+                           // </div>
+                            
+                           // <div className="form-group">
+                              // <label for="password" className="text">Password</label>
+                              // <input type="password" className="form-control" id="newPassword" placeholder="" required=""></input>
+                            //</div>
+
+                           // <div>
+                               // <label for="confirmPassword" className="text">Confirm Password</label>
+                               //<input type="password" className="form-control" id="confirmPassword" placeholder="" required="" onkeyup="checkPass(); return false;"></input>
+                           //</div>
+
+                           // <div>
+                               // <label for="dob" className="date">Birthday</label>
+                               // <input type="date" className="form-control" id="birthdate" placeholder="" required=""></input>
+                            //</div>
+
+                           //<div>
+                                //<label for="dob" className="text">Parents Email address</label>
+                                //<input type="email" className="form-control" id="email" placeholder=""></input>
+                           //</div>
+
+                           // <div className="form-check">
+                                //<Link className="btn btn-login float-right" id="btnSubmit" value="Submit" type="submit" to="/mathtype">
+                                   // Submit
+                                //</Link>
+                                
+                            //</div>
+                       // </form>
+                   // </div>
+
+                    //<div className="col-md-8 banner-sec">
+                       // <img id="logo" src={logo} alt="mathventure"></img>
+                   //</div>
+               // </div>
+            //</div>
+       //</section>
+    //</div>
+//;
+
+export default newPlayerForm;
