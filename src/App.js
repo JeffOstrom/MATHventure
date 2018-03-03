c68522a4e635cdce3fca48fed6ede72f67288dc7
Jeff Ostrom <j.ostrm@gmail.com>
2018-03-03 11:37:44 -0600

    Results page added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Correct from "./pages/Correct";
 import Incorrect from "./pages/Incorrect";
 import Subtraction from "./pages/Subtraction";
 import Level from "./pages/Level";
+import Results from "./pages/Results"
 
 
 const App = () => 
@@ -21,6 +22,7 @@ const App = () =>
             <Route exact path="/incorrect" component={Incorrect} />
             <Route exact path="/subtraction" component={Subtraction} />
             <Route exact path="/Level" component={Level} />
+            <Route exact path="/Results" component={Results} />
         </div>
     </Router>;
 
