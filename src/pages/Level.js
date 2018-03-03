0d632847d3d97730b8c0a5f4069ad0367f80d0d3
Jeff Ostrom <j.ostrm@gmail.com>
2018-03-03 11:18:45 -0600

    Select difficulty level working correctly

--- src/pages/Level.js
+++ src/pages/Level.js
@@ -2,19 +2,31 @@ import React, {Component} from "react";
 import { Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import "./assets/css/style.css";
+import "./addition.css"
 
 //Game difficulty level easy,medium, hard
 class Level extends Component {
       render(){
         return(
-              <div class="btn-group-vertical">
+              <div className="btn-group-vertical">
       
                 <h1> Select Difficulty Level</h1>
                         <br></br>
-
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Easy</button>
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Medium</button>
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Hard</button>
+                        <div className="col-8">
+                                    <Link type="button" className="btn btn-secondary btn-lg btn-block" to="/Addition">
+                                        Easy
+                                    </Link>
+                         </div>
+                         <div className="col-8">
+                                    <Link type="button" className="btn btn-secondary btn-lg btn-block" to="/Level">
+                                        Medium
+                                    </Link>
+                         </div>
+                         <div className="col-8">
+                                    <Link type="button" className="btn btn-secondary btn-lg btn-block" to="/Level">
+                                        Hard
+                                    </Link>
+                         </div>
               </div>
         );
    }
